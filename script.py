da9a42a9490b43b87e957e6d3747b46a45f49031
wazout <wazout@sudomail.com>
2021-06-03 18:22:04 -0700

    Updated scripts.py

--- script.py
+++ script.py
@@ -1,15 +1,7 @@
-import math
-import os
 import sys
-
-import requests
 
 name = input("Your Name? ")
 print("Hello", name)
 
-# print(sys.version)
-# print(sys.executable)
-
-
-# r = requests.get("https://michaelesteinmetz.com")
-# print(r.status_code)
+print(sys.version)
+print(sys.executable)
